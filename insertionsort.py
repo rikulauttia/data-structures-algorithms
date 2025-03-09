8836cc76d8cd9a1cd17932fec58081808a40e4d3
Riku Lauttia <riku.lauttia@gmail.com>
2025-03-09 13:45:06 +0200

    insertionsort demo

--- insertionsort.py
+++ insertionsort.py
@@ -1,1 +1,17 @@
 # checks element one by one sorts them in a gradually built-up sorted array. so it builds up sorted array one element by time.
+
+def insertionsort(array):
+    for i in range(1, len(array)):
+        current_number = array[i]
+        compare_number_position = i-1
+
+        while compare_number_position >= 0 and array[compare_number_position] > current_number:
+            array[compare_number_position+1]=array[compare_number_position]
+            compare_number_position -= 1
+        
+        array[compare_number_position+1] = current_number
+
+# Testing
+numbers = [2, 1, 7, 4, 8]
+insertionsort(numbers)
+print("Järjestetty lista:", numbers)
